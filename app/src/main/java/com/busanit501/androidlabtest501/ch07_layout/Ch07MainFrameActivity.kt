36306d2676d3babc141813ecac390d9beb1787cf
LeeHanSol <hslee4954@gmail.com>
2024-07-04 12:51:25 +0900

    안드로이드 화면 0704 조퇴

---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch07_layout/Ch07MainFrameActivity.kt
@@ -0,0 +1,63 @@
+package com.busanit501.androidlabtest501.ch07_layout
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityCh07MainFrameBinding
+
+class Ch07MainFrameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        //바인딩, 뷰들을 , 하나의 인스턴스에 담아서, 꺼내서 사용하기.
+        val binding = ActivityCh07MainFrameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.activity_ch07_main_frame)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val testBtn = binding.ch07FrameBtn1
+        val img1 = binding.ch07FrameImg1
+        val img2 = binding.ch07FrameImg2
+        val img3 = binding.ch07FrameImg3
+
+        val imgList : MutableList<ImageView> = mutableListOf<ImageView>(img1,img2,img3)
+
+        var imgCheck: Int = 0
+
+        testBtn.setOnClickListener {
+            //
+            if(imgCheck == 0) {
+                img1.visibility = View.VISIBLE
+                img2.visibility = View.INVISIBLE
+                img3.visibility = View.INVISIBLE
+                imgCheck++
+            } else if (imgCheck == 1) {
+                img1.visibility = View.INVISIBLE
+                img2.visibility = View.VISIBLE
+                img3.visibility = View.INVISIBLE
+                imgCheck++
+            }
+            else if (imgCheck == 2) {
+                img1.visibility = View.INVISIBLE
+                img2.visibility = View.INVISIBLE
+                img3.visibility = View.VISIBLE
+                imgCheck = 0;
+            }
+        }
+
+
+
+
+    }// onCreate
+}
---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch08_event/Ch08MainEvent1Activity.kt
@@ -0,0 +1,61 @@
+package com.busanit501.androidlabtest501.ch08_event
+
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+
+class Ch08MainEvent1Activity : AppCompatActivity() {
+
+    val TAG = "Ch08MainEvent1Activity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ch08_main_event1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }//onCreate
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        // key , 터치 확인, 수정
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                Log.d(TAG,"ACTION_DOWN : Down key 동작 확인,좌표 : x : ${event.x}, y:${event.y} , rawX : ${event.rawX}, rawY : ${event.rawY}")
+            }
+
+            MotionEvent.ACTION_UP -> {
+                Log.d(TAG,"ACTION_UP : Up key 동작 확인")
+            }
+        }
+
+        return super.onTouchEvent(event)
+
+    } // onTouchEvent
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when(keyCode) {
+            KeyEvent.KEYCODE_0 -> Log.d(TAG,"KEYCODE_0, 숫자 0번 동작 확인")
+            KeyEvent.KEYCODE_A -> Log.d(TAG,"KEYCODE_A, A 키 동작 확인")
+            KeyEvent.KEYCODE_ENTER -> Log.d(TAG,"KEYCODE_ENTER, enter 키 동작 확인")
+//            KeyEvent.KEYCODE_BACK -> Log.d(TAG,"KEYCODE_BACK, 뒤로가기 키 동작 확인")
+            KeyEvent.KEYCODE_VOLUME_UP -> Log.d(TAG,"KEYCODE_VOLUME_UP, 볼륨업 키 동작 확인")
+            KeyEvent.KEYCODE_VOLUME_DOWN -> Log.d(TAG,"KEYCODE_VOLUME_DOWN, 볼륨다운 키 동작 확인")
+        }
+        return super.onKeyDown(keyCode, event)
+    } //onKeyDown
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.d(TAG,"백키 수동으로 확인 해보기.")
+    }
+}
